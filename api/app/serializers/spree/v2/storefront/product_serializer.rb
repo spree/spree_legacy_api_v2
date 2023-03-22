spree/spree

Localized_slugs added to product endpoint

api/app/serializers/spree/v2/storefront/product_serializer.rb
@@ -24,6 +24,10 @@ module Spree
           product.available?
         end
 
+        attribute :translated_slugs do |product|
+          product.translated_slugs
+        end
+
         attribute :currency do |_product, params|
           params[:currency]
         end
@@ -43,6 +47,9 @@ module Spree
         attribute :display_compare_at_price do |product, params|
           display_compare_at_price(product, params[:currency])
         end
+
+
+
 
         has_many :variants
         has_many :option_types

spree/spree_legacy_api_v2

api/app/serializers/spree/v2/storefront/product_serializer.rb
@@ -24,6 +24,10 @@ module Spree
           product.available?
         end
 
+        attribute :localized_slugs do |product|
+          product.localized_slugs
+        end
+
         attribute :currency do |_product, params|
           params[:currency]
         end